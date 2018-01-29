b85f3ed358dc65dd036a349eed6e88061deda4c5
CarloMtzNST <carlo.martinez@nearshoretechnology.com>
2018-01-29 11:20:56 -0600

    Added support for Selenium jar instead of manually creating server.

--- Tests/conf.js
+++ Tests/conf.js
@@ -1,12 +1,14 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumServerJar: 'C:\\Users\\Carlo Martinez\\AppData\\Roaming\\npm\\node_modules\\protractor\\node_modules\\webdriver-manager\\selenium\\selenium-server-standalone-3.8.1.jar',
   specs: ['todo-spec.js'],
-  framework: 'jasmine2',
+  framework: 'jasmine',
   onPrepare: function() {
     var jasmineReporters = require('jasmine-reporters');
     jasmine.getEnv().addReporter(new jasmineReporters.NUnitXmlReporter({
         consolidateAll: true,
-        savePath: 'C:\\Source\\GIT\\Tutorials.Protractor\\testResults'
+        savePath: 'C:\\Source\\GIT\\Tutorials.Protractor\\testResults',
+        
     }));
   }
 };
---
+++ Tests/run.bat
@@ -0,0 +1,5 @@
+cd C:\Users\Carlo Martinez\AppData\Roaming\npm\node_modules\protractor\bin\
+webdriver-manager start
+SLEEP 10
+cd C:\Users\Carlo Martinez\OneDrive - NearShore Technology\Training\Protractor\Tests
+protractor conf.js
