bacb09c91b443e82d2c4b0a17edff4d10d3ae37d
NSTCarloMtz <Carlo.Martinez@nearshoretechnology.com>
2018-01-29 16:25:15 -0600

    refactor(conf.js): Changed selenium server path to work with Ubuntu.

--- Tests/conf.js
+++ Tests/conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   // seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumServerJar: 'C:\\Users\\Carlo Martinez\\AppData\\Roaming\\npm\\node_modules\\protractor\\node_modules\\webdriver-manager\\selenium\\selenium-server-standalone-3.8.1.jar',
+  seleniumServerJar: '../selenium-server-standalone-3.8.1.jar',
   specs: ['todo-spec.js'],
   framework: 'jasmine',
   onPrepare: function() {
