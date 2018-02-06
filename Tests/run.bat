defd2e0588f9cb26e8075edf483df9d92ddd0eb4
CarloMtzNST <carlo.martinez@nearshoretechnology.com>
2018-02-06 16:31:25 -0600

    chores(*): Removed unused files.

--- Tests/run.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\Carlo Martinez\AppData\Roaming\npm\node_modules\protractor\bin\
-webdriver-manager start
-SLEEP 10
-cd C:\Users\Carlo Martinez\OneDrive - NearShore Technology\Training\Protractor\Tests
-protractor conf.js
